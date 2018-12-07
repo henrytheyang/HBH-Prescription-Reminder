56597dd20b7d375700dbd5491c35f220721ea958
Henry Yang <contacthenryyang@gmail.com>
2018-12-07 05:24:31 -0800

    Add dynamically rendered drug & email

--- src/body/message.jsx
+++ src/body/message.jsx
@@ -5,7 +5,7 @@ function Message(props) {
   return (
     <div>
       <div>Hi {props.patient.first}!</div>
-      <div>Thanks for signing up for a reminder for medicine name. We'll send it to email address on {props.date} to give you a little notice.</div>
+      <div>Thanks for signing up for a reminder for {props.drug}. We'll send it to {props.patient.email} on {props.date} to give you a little notice.</div>
       <button>Set Reminder Button</button>
     </div>
   )
