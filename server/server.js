6607e9a1683fdde0596af4279d44c99aa14b6994
Henry Yang <contacthenryyang@gmail.com>
2018-12-07 04:55:03 -0800

    Serve static files

--- server/server.js
+++ server/server.js
@@ -2,5 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello, world!'));
+// app.get('/', (req, res) => res.send('Hello, world!'));
+app.use(express.static('dist'));
+
 app.listen(port, () => console.log(`Server is now listening on port ${port}!`));
