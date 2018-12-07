829e6414f5cb375e76c7d2229788b7a7ddada0de
Henry Yang <contacthenryyang@gmail.com>
2018-12-06 19:30:13 -0800

    Setup boilerplate webpack/react

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'Here is a react/webpack setup!';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
