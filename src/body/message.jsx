431df6bb7f12a87fb93f097d2bbdfadd6a278770
Henry Yang <contacthenryyang@gmail.com>
2018-12-08 06:15:41 -0800

    Create date calculator, add to state

--- src/body/message.jsx
+++ src/body/message.jsx
@@ -5,8 +5,8 @@ function Message(props) {
   return (
     <div>
       <div>Hi{props.firstName}!</div>
-      <div>Thanks for signing up for a reminder for {props.drugName}. We'll send it to {props.email} on {props.date} to give you a little notice.</div>
-      <button onClick={() => props.handleClick()}>Set Reminder Button</button>
+      <div>Thanks for signing up for a reminder for {props.drugName}. We'll send it to {props.email} on {props.reminderDate} to give you a little notice.</div>
+      <button onClick={() => props.handleClick()}>Set Reminder</button>
     </div>
   )
 }
--- src/body/primary.jsx
+++ src/body/primary.jsx
@@ -13,37 +13,49 @@ class Primary extends React.Component {
       firstName: '',
       email: '',
       drugName: '',
-      daysSupply: 60,
-      date: 'May 30',
+      daysSupply: 89,
+      reminderDate: '',
       symptoms: [
         'Happiness', 'Sleepiness', 'Grumpiness',
       ],
     };
   }
 
-  handleTextChange(event) {
+  handleChange(event) {
     console.log(`${event.target.value} was inputted`);
     this.setState({[event.target.name]: event.target.value});
   }
 
   splitNames(string) {
     let names = string.split(' ');
-    // console.log(`${names[0]} was split off`);
     // console.log(`names[0] = ${names[0]}`);
     let first = ' ' + names[0];
     this.setState({firstName: first});
   }
 
+  calculateDate() {
+    //Calculate date, then setState the date
+    // date that is current day plus days supply - 5 days
+    let today = Date.now();
+    // console.log(`today = ${today}`);
+    // ms * s * min * hr * day
+
+    // Check math- discrepancy when going from 90 to 100 days
+    let emailingDay =   1000 * 60 * 60 * 24 * (this.state.daysSupply - 5) + today;
+    let stringifiedDate = new Date(emailingDay).toString();
+    let arrayEmailingDay = stringifiedDate.split(' ');
+    let stringifiedSimplifiedDate = `${arrayEmailingDay[1]} ${arrayEmailingDay[2]}`;
+    console.log(`stringifiedSimplifiedDate = ${stringifiedSimplifiedDate}`);
+    this.setState({reminderDate: stringifiedSimplifiedDate});
+  }
+
   handleClick() {
-    console.log('button was clicked');
+    // console.log('button was clicked');
     this.splitNames(this.state.patientName);
     // ajax call for symptoms
     // this.setState to update symptoms, update isSubmitted
+    this.calculateDate();
     this.setState({isSubmitted: true});
-  }
-
-  calculateDate() {
-    //Calculate date, then setState the date
   }
 
   render() {
@@ -58,8 +70,8 @@ class Primary extends React.Component {
     }
     return (
       <div id='primary'>
-        <Input handleTextChange={this.handleTextChange.bind(this)}/>
-        <Message firstName={this.state.firstName} email={this.state.email} date={this.state.date} drugName={this.state.drugName} handleClick={this.handleClick.bind(this)}/>
+        <Input handleChange={this.handleChange.bind(this)}/>
+        <Message firstName={this.state.firstName} email={this.state.email} reminderDate={this.state.reminderDate} drugName={this.state.drugName} handleClick={this.handleClick.bind(this)}/>
         {postSubmission}
       </div>
     )
