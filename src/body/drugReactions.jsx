6d6579646c20bd3c40ac4ba852deb53137d8bbb6
Henry Yang <contacthenryyang@gmail.com>
2018-12-08 07:51:43 -0800

    Hide if not submitted

--- src/body/drugReactions.jsx
+++ src/body/drugReactions.jsx
@@ -2,13 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function DrugReactions(props) {
+  let isSubmitted = props.isSubmitted;
+  let postSubmission;
+  if (isSubmitted) {
+    postSubmission = (
+      <div>
+        <div>As a service, here are the possible drug reactions you should contact your doctor if you're experiencing:</div>
+        {props.symptoms.map(item => (
+            <div>*{item}</div>
+          ))
+        }
+      </div>
+    )
+  }
   return (
     <div>
-      <div>As a service, here are the possible drug reactions you should contact your doctor if you're experiencing:</div>
-      {props.symptoms.map(item => (
-          <div>*{item}</div>
-        ))
-      }
+      {postSubmission}
     </div>
   )
 }
--- src/body/message.jsx
+++ src/body/message.jsx
@@ -2,10 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function Message(props) {
+  let isSubmitted = props.isSubmitted;
+  let postSubmission;
+  if (isSubmitted) {
+    postSubmission = (
+      <div>
+        <div>Hi {props.firstName}!</div>
+        <div>Thanks for signing up for a reminder for {props.drugName}. We'll send it to {props.email} on {props.reminderDate} to give you a little notice.</div>
+      </div>
+    );
+  }
   return (
     <div>
-      <div>Hi{props.firstName}!</div>
-      <div>Thanks for signing up for a reminder for {props.drugName}. We'll send it to {props.email} on {props.reminderDate} to give you a little notice.</div>
+      {postSubmission}
       <button onClick={() => props.handleClick()}>Set Reminder</button>
     </div>
   )
